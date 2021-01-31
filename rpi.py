6a5411fa00b3b81d015fd6e78a0cfec2dcaaf189
Kyle Chesney <kyle.2493@gmail.com>
2021-01-31 00:41:54 -0500

    Integrate MQTT into rpi script, rename it

--- rpi.py
+++ rpi.py
@@ -2,6 +2,7 @@
 
 from time import sleep
 from gpiozero import LED, MotionSensor
+from db_interface import *
 
 # Initialize objects for I/O devices, assign GPIO pins
 laser = LED(17)
@@ -10,7 +11,7 @@ pir = MotionSensor(4)
 while True:
     # Note: pir.wait_for_motion() causes program to become unexitable
     if pir.motion_detected:
-        # TODO: log time in a database
+        add_timestamp(conn, location_id)
         laser.on()
         sleep(5*60)
     else:
