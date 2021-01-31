6f3a08fe98356dfd9b7b103940762b6f4fa3e67e
Kyle Chesney <kyle.2493@gmail.com>
2021-01-30 22:22:16 -0500

    Add db logging to mqtt_server.py Add __init__ file to allow lazy import dev laziness, not the tech kind

--- mqtt_server.py
+++ mqtt_server.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
+from db_interface import  *
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with code: {}".format(str(rc)))
+    # Here's a quick way to sub to multiple topics with one server
+    # in case you wanted to do something actually useful
     topics = ["toilaser"]
     [client.subscribe(topic) for topic in topics]
 
 
 def on_message(client, userdata, msg):
     print("message received")
-    print(str(msg.payload))
-    # TODO: log to database
+    print(str(msg.payload.decode("utf-8")))
+    location_id = int(msg.payload.decode("utf-8"))
+    conn = create_connection()
+    add_timestamp(conn, location_id)
+    conn.close()
 
 
 def mqtt_init():
